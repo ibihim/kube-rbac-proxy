2d00f206040f697db34655dfc734d29d610ba6d3
Krzysztof Ostrowski <kostrows@redhat.com>
2022-10-09 15:29:14 +0200

    pkg/filters: small withAuthHeaders change
    Signed-off-by: Krzysztof Ostrowski <kostrows@redhat.com>

--- pkg/filters/authn.go
+++ pkg/filters/authn.go
@@ -35,15 +35,17 @@ func WithAuthentication(handler http.Handler, authReq authenticator.Request, aud
 // WithAuthHeaders adds identity information to the headers.
 // Must not be used, if connection is not encrypted with TLS.
 func WithAuthHeaders(handler http.Handler, cfg *authn.AuthnHeaderConfig) http.Handler {
+	if !cfg.Enabled {
+		return handler
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if cfg.Enabled {
-			u, ok := request.UserFrom(req.Context())
-			if ok {
-				// Seemingly well-known headers to tell the upstream about user's identity
-				// so that the upstream can achieve the original goal of delegating RBAC authn/authz to kube-rbac-proxy
-				req.Header.Set(cfg.UserFieldName, u.GetName())
-				req.Header.Set(cfg.GroupsFieldName, strings.Join(u.GetGroups(), cfg.GroupSeparator))
-			}
+		u, ok := request.UserFrom(req.Context())
+		if ok {
+			// Seemingly well-known headers to tell the upstream about user's identity
+			// so that the upstream can achieve the original goal of delegating RBAC authn/authz to kube-rbac-proxy
+			req.Header.Set(cfg.UserFieldName, u.GetName())
+			req.Header.Set(cfg.GroupsFieldName, strings.Join(u.GetGroups(), cfg.GroupSeparator))
 		}
 
 		handler.ServeHTTP(w, req)
